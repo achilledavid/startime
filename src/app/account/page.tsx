bd7aa33c67a0953bde3e97757d1494c27e3f3313
Achille DAVID <davidachille18@gmail.com>
2025-06-20 16:17:03 +0200

    fix(account): removed organization controls

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,6 @@
 import SignOut from "@/components/auth/sign-out";
 import { authenticate } from "@/lib/session";
-import MyOrganization from "./my-ogranization";
+import MyOrganization from "./my-organization";
 
 export default async function Account() {
   const session = await authenticate()
@@ -8,7 +8,7 @@ export default async function Account() {
   return (
     <main className="p-4 flex flex-col gap-4">
       <p>Hello {session.user.name}!</p>
-      <MyOrganization user={session.user} />
+      <MyOrganization />
       <SignOut />
     </main>
   )
