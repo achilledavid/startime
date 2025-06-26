85efbb9e2874ffe8cee4b898b9828b1e45d4071e
Achille DAVID <davidachille18@gmail.com>
2025-06-26 19:21:23 +0200

    fix(onboarding): added header to creation

--- src/app/[slug]/admin/onboardings/new/page.tsx
+++ src/app/[slug]/admin/onboardings/new/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import OnboardingForm from "@/components/onboarding/form";
+import { Separator } from "@/components/ui/separator";
 import { useOrganization } from "@/contexts/organization";
 import { useRouter } from "next/navigation";
+import { Fragment } from "react";
 
 export default function Page() {
     const { data } = useOrganization()
@@ -11,10 +13,19 @@ export default function Page() {
     if (!data) return
 
     return (
-        <div className="p-8 w-full h-full">
-            <OnboardingForm onSuccess={() => {
-                router.push(`/${data.organization.slug}/admin/onboardings`)
-            }} />
-        </div>
+        <Fragment>
+            <div className="p-6">
+                <div>
+                    <h1 className="text-2xl font-semibold tracking-tight">Create a new onboarding</h1>
+                    <p className="text-muted-foreground">Design an onboarding experience that will help your new team members hit the ground running.</p>
+                </div>
+            </div>
+            <Separator />
+            <div className="p-4 w-full">
+                <OnboardingForm onSuccess={() => {
+                    router.push(`/${data.organization.slug}/admin/onboardings`)
+                }} />
+            </div>
+        </Fragment>
     )
 }
